a3c172563957a20aaa91a2243d5c8d37adb50b53
gitdoallan <mailtoallan@icloud.com>
2022-03-22 16:56:59 -0300

    requisitos 1 e 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import About from '../components/About';
+
+describe('Teste se a página contém as informações sobre a Pokédex.', () => {
+  beforeEach(() => render(<About />));
+  it('Teste se a página contém um heading h2 com o texto About Pokédex.', () => {
+    const heading = screen.getByRole('heading', { level: 2, name: 'About Pokédex' });
+    expect(heading).toBeInTheDocument();
+  });
+  it('Teste se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const p = screen.getAllByText(/pokémons/i);
+    expect(p).toHaveLength(2);
+  });
+  it('Teste se a página contém a seguinte imagem de uma Pokédex:', () => {
+    const img = screen.getByRole('img');
+    expect(img).toBeInTheDocument();
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithTouter';
+import App from '../App';
+
+describe('1. Teste se o topo da aplicação contém um conjunto fixo de links', () => {
+  beforeEach(() => {
+    renderWithRouter(<App />);
+  });
+  it('O primeiro link deve possuir o texto Home.', () => {
+    const link = screen.getByText('Home');
+    expect(link).toBeInTheDocument();
+  });
+  it('O segundo link deve possuir o texto About.', () => {
+    const link = screen.getByText('About');
+    expect(link).toBeInTheDocument();
+  });
+  it('O terceiro link deve possuir o texto Favorite Pokémons.', () => {
+    const link = screen.getByText('Favorite Pokémons');
+    expect(link).toBeInTheDocument();
+  });
+});
